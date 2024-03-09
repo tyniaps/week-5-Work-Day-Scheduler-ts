093109d9d07134f25a8b13be679c4ecf29426d9e
KnightNimbus <ty.s.spain@gmail.com>
2024-03-09 21:01:51 +0000

    created function in order to display current day (referencing day js)

--- Develop/script.js
+++ Develop/script.js
@@ -1,9 +1,16 @@
+var dayDisplayEl = $('#dayDisplay');
+
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
 $(function () {
 
-  
+  function displayDay() {
+    var currentDay = dayjs().format('dddd DD MMMM');
+    dayDisplayEl.text(currentDay);
+  }
+
+
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
